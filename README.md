bc14b409a753a6f6d397978ea80fab18f54b1b33
hypernova1 <chtlstjd01@gmail.com>
2020-07-15 22:46:50 +0900

    properties 파일로 설정 분리

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 자바로 웹 서버 구현
 
 #### SSL 활성화 방법
-프로그램 Argument 의 첫 번째 인자에 keyStore 파일명, 두 번째 인자에 비밀번호를 넣고 실행
+resources/config/application.properties
+* keyStore: keyStore 파일명
+* keyStorePassword: keyStore 비밀번호
--- src/main/java/org/sam/server/HttpServer.java
+++ src/main/java/org/sam/server/HttpServer.java
@@ -1,17 +1,15 @@
 package org.sam.server;
 
-import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSocket;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
+import java.util.Properties;
 
 public class HttpServer implements Runnable {
 
-    private static final int PORT = 8080;
     public static boolean verbose = true;
 
     private final Socket connect;
@@ -20,23 +18,28 @@ public class HttpServer implements Runnable {
         this.connect = connect;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Properties properties = getProperties();
+        String keyStore = properties.getProperty("keyStore");
+        String password = properties.getProperty("keyStorePassword");
+        int port = Integer.parseInt(properties.getProperty("server.port"));
 
         try {
 
             ServerSocket serverSocket = null;
 
-            if (args[0] != null) {
-                System.setProperty("javax.net.ssl.keyStore", args[0]);
-                System.setProperty("javax.net.ssl.keyStorePassword", args[1]);
+            if (keyStore != null) {
+                System.setProperty("javax.net.ssl.keyStore", keyStore);
+                System.setProperty("javax.net.ssl.keyStorePassword", password);
                 System.setProperty("javax.net.debug", "ssl");
 
                 SSLServerSocketFactory sslserversocketfactory =
                         (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
                 serverSocket =
-                        sslserversocketfactory.createServerSocket(PORT);
+                        sslserversocketfactory.createServerSocket(port);
             } else {
-                serverSocket = new ServerSocket(PORT);
+                serverSocket = new ServerSocket(port);
             }
 
             if (verbose) {
@@ -45,7 +48,6 @@ public class HttpServer implements Runnable {
 
             while (true) {
                 HttpServer httpServer = new HttpServer(serverSocket.accept());
-
                 if (verbose) {
                     System.out.println("connected.." + new Date());
                 }
@@ -55,6 +57,16 @@ public class HttpServer implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        InputStream resourceAsStream = HttpServer.class.getClassLoader().getResourceAsStream("config/application.properties");
+
+        if (resourceAsStream != null) {
+            properties.load(resourceAsStream);
+        }
+        return properties;
     }
 
     public void run() {
