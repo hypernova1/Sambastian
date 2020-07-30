67860ba63e3a11bc3f884f4b59f1239ff01aeacf
hypernova1 <hypemova@gmail.com>
2020-07-30 13:27:21 +0900

    Response 리팩토링

--- src/main/java/org/sam/server/http/HttpResponse.java
+++ src/main/java/org/sam/server/http/HttpResponse.java
@@ -16,17 +16,8 @@ import java.util.Set;
  * Date: 2020/07/17
  * Time: 1:34 PM
  */
-public class HttpResponse {
+public class HttpResponse extends Response {
 
-    private final ClassLoader classLoader = getClass().getClassLoader();
-
-    private static final String DEFAULT_FILE = "static/index.html";
-    private static final String BAD_REQUEST = "static/400.html";
-    private static final String FILE_NOT_FOUND = "static/404.html";
-    private static final String METHOD_NOT_SUPPORTED = "static/not_supported";
-
-    private final PrintWriter writer;
-    private final BufferedOutputStream outputStream;
     private final Map<String, Object> headers = new HashMap<>();
     private Set<Cookie> cookies = CookieStore.getCookies();
     private final String requestPath;
@@ -35,9 +26,11 @@ public class HttpResponse {
     private HttpStatus httpStatus;
     private String contentMimeType;
 
+    private byte[] fileData = new byte[1024];
+    private int fileLength;
+
     public HttpResponse(OutputStream os, String path) {
-        this.writer = new PrintWriter(os);
-        this.outputStream = new BufferedOutputStream(os);
+        super(os);
         this.requestPath = path;
     }
 
@@ -48,32 +41,25 @@ public class HttpResponse {
     public void execute(String filePath, HttpStatus status) {
         this.httpStatus = status;
 
-        int fileLength;
         try {
             if (getContentMimeType().equals(ContentType.APPLICATION_JSON.getValue())) {
-                fileLength = readJson(filePath);
-            } else  fileLength = loadStaticFile(filePath);
-
-            headers.put("Server", "Java HTTP Server from sam : 1.0");
-            headers.put("Date", LocalDateTime.now());
-            headers.put("Content-Type", getContentMimeType());
-            headers.put("Content-length", fileLength);
+                this.fileLength = readJson(filePath);
+            } else  this.fileLength = readStaticResource(filePath);
 
             printHeader();
 
+            outputStream.write(fileData, 0, this.fileLength);
             CookieStore.vacateList();
             writer.flush();
             outputStream.flush();
+            outputStream.close();
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void addCookies(Cookie cookie) {
-        this.cookies.add(cookie);
-    }
-
-    private int loadStaticFile(String filePath) throws IOException {
+    private int readStaticResource(String filePath) throws IOException {
         InputStream fis = classLoader.getResourceAsStream(filePath);
         File staticFile = new File("src/main" + filePath);
 
@@ -82,26 +68,20 @@ public class HttpResponse {
             return 0;
         }
 
-        byte[] fileData = new byte[1024];
         int fileLength;
         if (staticFile.exists()) {
-            fileLength = (int) staticFile.length();
-            fileData = readFileData(staticFile, fileLength);
+            fileLength = readFileData(staticFile);
         } else {
             assert fis != null;
             fileLength = fis.read(fileData);
         }
 
-        outputStream.write(fileData, 0, fileLength);
-
         return fileLength;
     }
 
-    private byte[] readFileData(File file, int fileLength) throws IOException {
-        byte[] fileData = new byte[fileLength];
+    private int readFileData(File file) throws IOException {
         FileInputStream fileIn = new FileInputStream(file);
-        fileIn.read(fileData);
-        return fileData;
+        return fileIn.read(this.fileData);
     }
 
     private int readJson(String json) throws IOException {
@@ -115,6 +95,10 @@ public class HttpResponse {
 
     private void printHeader() {
         writer.println("HTTP/1.1 " + httpStatus.getCode() + " " + httpStatus.getMessage());
+        headers.put("Server", "Java HTTP Server from sam : 1.0");
+        headers.put("Date", LocalDateTime.now());
+        headers.put("Content-Type", getContentMimeType());
+        headers.put("Content-length", fileLength);
         headers.keySet().forEach(key -> writer.println(key + ": " + headers.get(key)));
         printCookies();
         writer.println();
@@ -155,22 +139,6 @@ public class HttpResponse {
         execute(BAD_REQUEST, HttpStatus.BAD_REQUEST);
     }
 
-    public void methodNotImplemented() throws IOException {
-        if (!HttpServer.verbose) {
-            System.out.println("501 not implemented :" + requestPath + "method");
-        }
-
-        execute(METHOD_NOT_SUPPORTED, HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    public void returnIndexFile() throws IOException {
-        if (this.requestPath.endsWith("/")) {
-            filePath = DEFAULT_FILE;
-        }
-
-        execute(filePath, HttpStatus.OK);
-    }
-
     public void setContentMimeType(ContentType contentMimeType) {
         this.contentMimeType = contentMimeType.getValue();
     }
@@ -187,6 +155,10 @@ public class HttpResponse {
         return ContentType.TEXT_PLAIN.getValue();
     }
 
+    public void addCookies(Cookie cookie) {
+        this.cookies.add(cookie);
+    }
+
     public void setHeader(String key, String value) {
         this.headers.put(key, value);
     }
@@ -201,6 +173,22 @@ public class HttpResponse {
 
     public void getStaticResources() {
         String filePath = requestPath.replace("/resources", "/resources/static");
+        execute(filePath, HttpStatus.OK);
+    }
+
+    public void methodNotImplemented() throws IOException {
+        if (!HttpServer.verbose) {
+            System.out.println("501 not implemented :" + requestPath + "method");
+        }
+
+        execute(METHOD_NOT_SUPPORTED, HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public void returnIndexFile() throws IOException {
+        if (this.requestPath.endsWith("/")) {
+            filePath = DEFAULT_FILE;
+        }
+
         execute(filePath, HttpStatus.OK);
     }
 }
---
+++ src/main/java/org/sam/server/http/Response.java
@@ -0,0 +1,28 @@
+package org.sam.server.http;
+
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+/**
+ * Created by melchor
+ * Date: 2020/07/30
+ * Time: 9:20 AM
+ */
+public abstract class Response {
+
+    protected final ClassLoader classLoader = getClass().getClassLoader();
+
+    protected static final String DEFAULT_FILE = "static/index.html";
+    protected static final String BAD_REQUEST = "static/400.html";
+    protected static final String FILE_NOT_FOUND = "static/404.html";
+    protected static final String METHOD_NOT_SUPPORTED = "static/not_supported";
+
+    protected final PrintWriter writer;
+    protected final BufferedOutputStream outputStream;
+
+    public Response(OutputStream os) {
+        this.writer = new PrintWriter(os);
+        this.outputStream = new BufferedOutputStream(os);
+    }
+}
